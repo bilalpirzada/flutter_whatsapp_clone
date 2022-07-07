bd946d3c6cbef196c75b9fa8ca88ebac3ba70aec
92306 <bilal.se74@gmail.com>
2022-07-07 10:01:55 +0500

    used flutter icons instead of SVG image icons in chat card

--- lib/CustomUI/CustomChatCard.dart
+++ lib/CustomUI/CustomChatCard.dart
@@ -5,25 +5,33 @@ class CustomChatCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(
-        radius: 30,
-      ),
-      trailing: Text("10:00 PM"),
-      title: Text(
-        "Muhammad Bilal",
-        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-      ),
-      subtitle: Row(children: [
-        Icon(Icons.done_all),
-        SizedBox(
-          width: 3,
+    return InkWell(
+      onTap: () {},
+      child: ListTile(
+        leading: CircleAvatar(
+            radius: 30,
+            child: Icon(
+              Icons.groups,
+              color: Colors.white,
+              size: 37.0,
+            ),
+            backgroundColor: Colors.blueGrey),
+        trailing: Text("10:00 PM"),
+        title: Text(
+          "Muhammad Bilal",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
         ),
-        Text(
-          "Hi I am Bilal",
-          style: TextStyle(fontSize: 13),
-        )
-      ]),
+        subtitle: Row(children: [
+          Icon(Icons.done_all),
+          SizedBox(
+            width: 3,
+          ),
+          Text(
+            "Hi I am Bilal",
+            style: TextStyle(fontSize: 13),
+          )
+        ]),
+      ),
     );
   }
 }
